ef443540becaa04249877114fd4aacccad3f5fd9
kaande <kaande@ttu.ee>
2019-03-24 22:48:53 +0200

    ex07

--- src/collections/simulator/Simulator.java
+++ src/collections/simulator/Simulator.java
@@ -6,7 +6,7 @@ import static collections.simulator.HandType.*;
 public class Simulator {
 
     private Map<HandType, Integer> frequencyMap;
-    private int numberOfSimulations = 750000;
+    private int numberOfSimulations = 60000;
 
     public Simulator() {
 
